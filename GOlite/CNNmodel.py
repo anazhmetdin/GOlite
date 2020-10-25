e97ce0ea39be8f75c6589e6f75ad18f96d55c646
anegm98 <anegm98.eg@gmail.com>
2020-10-25 06:06:14 +0200

    use generator in CNN

--- GOlite/CNNmodel.py
+++ GOlite/CNNmodel.py
@@ -1,36 +1,73 @@
-from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Conv1D
 from keras.layers import Dense
+from GOlite.generator import DataGenerator
+import glob
 
 
 class CNNmodel():
-    def __init__(self, filters, filterSize):
+    def __init__(self, dPrefix, lPrefix, dim, label_dim, batchS, val,
+                 filters, filterSize):
         self.model = Sequential()
         self.filters = filters
-        self.fiterSize = filterSize.split(',')
+        self.filterSize = [int(i) for i in filterSize.split(',')]
+        self.dPrefix = dPrefix
+        self.lPrefix = lPrefix
+        self.dim = [int(i) for i in dim.split(',')]
+        self.label_dim = [int(i) for i in label_dim.split(',')]
+        self.batchS = batchS
+        self.val = val
+        self.list_IDs = dict()
+        self.list_IDs['train'] = list()
+        self.list_IDs['validation'] = list()
+        self.labels = dict()
+        self.generate_dicts()
         self.build_model()
-        self.X_train = []
-        self.X_test = []
-        self.y_train = []
-        self.y_test = []
 
     def build_model(self):
         a = self.filterSize[0]
         step = self.filterSize[1]
         b = self.filterSize[2]
+        print(self.filters, type(self.filters))
+        print(a, type(a))
+        print(self.batchS, type(self.batchS))
+        print(self.dim[1], type(self.dim[1]))
         self.model.add(Conv1D(filters=self.filters, kernel_size=a,
-                              activation='relu', input_shape=(None, 21, 2000)))
+                       strides=self.batchS, activation='relu',
+                       input_shape=(None, self.dim[1])))
         for size in range(a+step, b+1, step):
             self.model.add(Conv1D(filters=self.filters, kernel_size=size,
                                   activation='relu'))
-        self.model.add(Dense(32, activation='sigmoid'))
+        self.model.add(Dense(self.label_dim[1], activation='sigmoid'))
         self.model.compile(optimizer='adam',
-                           loss='sparse_categorical_crossentropy',
-                           metrics=['sparse_categorical_crossentropy'])
+                           loss='categorical_crossentropy',
+                           metrics=['categorical_crossentropy'])
 
-    def setup_data(self, data, testSize=0.1):
-        self.X_train, self.X_test,
-        self.y_train, self. y_test = train_test_split(seqData, functionData,
-                                                      test_size=testSize,
-                                                      random_state=42)
+    def generate_dicts(self):
+        iList = glob.glob(self.dPrefix)
+        oList = glob.glob(self.lPrefix)
+        for i in range(int(self.val*len(iList))-1):
+            self.list_IDs['train'].append(iList[i])
+            self.labels[iList[i]] = oList[i]
+        for i in range(int(self.val*len(iList)), len(iList)):
+            self.list_IDs['train'].append(iList[i])
+            self.labels[iList[i]] = oList[i]
+
+    def fit_model(self, testSize=0.1):
+        # Parameters
+        params = {'dim': tuple(self.dim),
+                  'label_dim': tuple(self.label_dim),
+                  'batch_size': self.batchS,
+                  'n_channels': 1,
+                  'shuffle': True}
+        # Datasets
+        partition = self.list_IDs
+        labels = self.labels
+        # Generators
+        training_generator = DataGenerator(partition['train'], labels,
+                                           **params)
+        validation_generator = DataGenerator(partition['validation'],
+                                             labels, **params)
+        self.model.fit(x=training_generator,
+                       validation_data=validation_generator,
+                       use_multiprocessing=True, workers=6)
--- GOlite/cli.py
+++ GOlite/cli.py
@@ -1,22 +1,42 @@
 """Console script for GOlite."""
 import argparse
 import sys
-
+from GOlite.CNNmodel import CNNmodel
 
 def main():
     """Console script for GOlite."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
     parser.add_argument("-f", "--filters", default="512",
                         help="filters count, default is 512")
     parser.add_argument("-s", "--filterSize", default="8,128,8",
                         help="start fiter size, end filter size, step size. \
                         Defualt is 8,128,8")
+    parser.add_argument("-d", "--dPrefix", required=True,
+                        help="files name pattern that only includes the data")
+    parser.add_argument("-l", "--lPrefix", required=True,
+                        help="files name pattern that only includes the labels")
+    parser.add_argument("-i", "--dDim", default="1000,58000", required=True,
+                        help="data dimension; default is 1000,58000")
+    parser.add_argument("-I", "--lDim", default="1000,500", required=True,
+                        help="label dimension; Defualt is 1000,500")
+    parser.add_argument("-b", "--batchSize", default="32",
+                        help="batch size of the generator")
+    parser.add_argument("-v", "--validation", default="0.2",
+                        help="validation percentage of the data")
     args = parser.parse_args()
 
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "GOlite.cli.main")
+    filters = int(args.filters)
+    filterSize = args.filterSize
+    dPrefix = args.dPrefix
+    lPrefix = args.lPrefix
+    dDim = args.dDim
+    lDim = args.lDim
+    batchSize = int(args.batchSize)
+    validation = float(args.validation)
+
+    CNN = CNNmodel(dPrefix, lPrefix, dDim, lDim, batchSize, validation,
+                   filters, filterSize)
+    CNN.fit_model()
     return 0
 
 
